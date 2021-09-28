7d79c2026703c8a61867d9fe6f07dbafdcf495a2
BlondeBurrito <edmw93@outlook.com>
2021-09-28 20:16:55 +0100

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
-//! This library is an implementation of the A-Star pathfinding algorithm tailored for traversing a bespoke collection of weighted hexagons. It's intended to calculate the most optimal path to a target hexagon where you are traversing from the center of one hexagon to the next along a line orthogonal to a hexagon edge.
+//! This library is an implementation of the A-Star pathfinding algorithm tailored for traversing a bespoke
+//! collection of weighted hexagons. It's intended to calculate the most optimal path to a target
+//! hexagon where you are traversing from the center of one hexagon to the next along a line orthogonal to a hexagon edge.
 //!
 //! The calculations are dpendent on the layout of your hexagon grid.
 //!
 //! ## Hexagon Layout/Orientation
 //!
-//! There are different ways in which a hexagon grid can be portrayed which in turn affects the discoverable neighbouring hexagons for path traversal. This library assumes that all hexagons have been plotted across a plane where the origin points sits at the bottom left - a deviation from this and the calcualtion simply won't work. Additionally a hexagon is herbey referred to as a 'node'.
+//! There are different ways in which a hexagon grid can be portrayed which in turn affects the
+//! discoverable neighbouring hexagons for path traversal. This library assumes that all hexagons have
+//! been plotted across a plane where the origin points sits at the bottom left - a deviation from this
+//! and the calcualtion simply won't work. Additionally a hexagon is herbey referred to as a 'node'.
 //!
-//! Each node has a label defining its position, known as `(column, row)`. NB: the `column` and `row` values can never be negative.
+//! Each node has a label defining its position, known as `(column, row)`.
+//! NB: the `column` and `row` values can never be negative.
 //!
 //! ### Flat Topped - odd columns shifted up
 //! 
@@ -24,9 +30,12 @@
 //!    \_______/         \_______/
 //! ```
 //!
-//! The column shift changes how we discover nearby nodes. For instance if we take the node at (0,0) and wish to discover the node to its North-East, (1,0), we can simply increment the `column` value by one.
+//! The column shift changes how we discover nearby nodes. For instance if we take the node at
+//! (0,0) and wish to discover the node to its North-East, (1,0), we can simply increment the `column` value by one.
 //!
-//! However if we take the node (1,0) and wish to discover its North-East node at (2,1) we have to increment both the `column` value and the `row` value. I.e the calculation changes depending on whether the odd column has been shifted up or down.
+//! However if we take the node (1,0) and wish to discover its North-East node at (2,1) we have
+//! to increment both the `column` value and the `row` value. I.e the calculation changes depending
+//!  on whether the odd column has been shifted up or down.
 //!
 //! In full for a node in an even column we can calculate a nodes neighbours thus:
 //!
@@ -66,9 +75,12 @@
 //!             \_______/
 //! ```
 
-//! The column shift changes how we discover nearby nodes. For instance if we take the node at (0,0) and wish to discover the node to its North-East, (1,1), we increment the `column` and `row` values by one.
+//! The column shift changes how we discover nearby nodes. For instance if we take the node at
+//! (0,0) and wish to discover the node to its North-East, (1,1), we increment the `column` and
+//! `row` values by one.
 //!
-//! However if we take the node (1,1) and wish to discover its North-East node at (2,1) we have to only increment the `column` value by one.
+//! However if we take the node (1,1) and wish to discover its North-East node at (2,1) we have to
+//! only increment the `column` value by one.
 //!
 //! In full for a node in an even column we can calculate a nodes neighbours thus:
 //!
